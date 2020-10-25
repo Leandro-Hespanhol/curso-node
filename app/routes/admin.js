1d3f2d404c358cf1edca084b0d3823e3bc26702a
Leandro Hespanhol <leandrohespanhol@gmail.com>
2020-10-25 19:51:23 -0300

    segunda

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,7 +2,7 @@ module.exports = (application) => {
   application.get('/formulario_inclusao_noticia', function (req, res) {
     res.render('admin/form_add_noticia');
   });
-
+  console.log(application);
   application.post('/noticias/salvar', function (req, res) {
     var noticia = req.body;
     console.log(noticia);
